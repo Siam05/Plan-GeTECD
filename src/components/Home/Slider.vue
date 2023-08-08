e86da1c06df8244d3996dd1265ba11bccadaecef
Siam05 <58203992+Siam05@users.noreply.github.com>
2023-08-08 18:41:46 +0600

    home

--- src/components/Home/Slider.vue
+++ src/components/Home/Slider.vue
@@ -1,7 +1,15 @@
 <template>
     <div>
         <!-- Mobile -->
-        <div class="imageStyle flex lg:hidden" :style="{backgroundImage:`url(${slider.image_mobile})`}"></div>
+        <div class="imageStyle flex lg:hidden" :style="{backgroundImage:`url(${slider.image_mobile})`}">
+            <div class="h-full text-left flex flex-col items-end justify-end">
+                <div class="px-4 sm:px-12 pb-16">
+                    <p data-aos="fade-right" class="text-xl md:text-3xl text-primary p-4 bg-white text-font-veener desc-width">PLAN INTERNATIONAL BANGLADESH</p>
+                    <p data-aos="fade-left" class="mt-4 text-xl sm:text-2xl lg:text-3xl font-semibold p-4 bg-primary text-white">{{ slider.title }}</p>
+                    <p data-aos="fade-right" class="mt-4 p-4 bg-tertiary text-white text-lg desc-width">{{ slider.description }}</p>
+                </div>
+            </div>
+        </div>
 
         <!-- Desktop -->
         <div class="imageStyle hidden lg:flex" :style="{backgroundImage:`url(${slider.image})`}">
